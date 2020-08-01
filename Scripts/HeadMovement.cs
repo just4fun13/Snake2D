8bd62622f6385f96bd62ef4953622d369e8a8056
just4fun13 <66634008+just4fun13@users.noreply.github.com>
2020-08-02 02:11:55 +0900

    Add files via upload

---
+++ Scripts/HeadMovement.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeadMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    [SerializeField]
+    float speed = 1.0f;
+
+    [SerializeField]
+    GameObject foodPrefab;
+
+    Transform lastNode;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.up * speed;
+        lastNode = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") != 0f)
+        {
+            transform.Rotate (0f, 0f, -180f * Time.deltaTime * Input.GetAxis("Horizontal"));
+            rb.velocity = transform.up * speed;
+        }   
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Food"))
+        {
+            if (lastNode == transform)
+                col.gameObject.tag = "Head";
+            else
+                StartCoroutine(SetTagWithDelay(col.transform));
+            col.transform.GetComponent<Node>().Init(lastNode);
+            lastNode = col.transform;
+            GenerateFood();
+        }
+
+        if (col.gameObject.CompareTag("Border") || col.gameObject.CompareTag("Tale"))
+        {
+            Debug.Log ("Crashed on " + col.gameObject.tag );
+            Time.timeScale = 0.0f;
+        }
+    }
+
+    IEnumerator SetTagWithDelay(Transform tr)
+    {
+        yield return new WaitForSeconds(0.5f);
+        tr.gameObject.tag = "Tale";
+        yield return null;
+    }
+
+    void GenerateFood()
+    {
+        Vector3 randomPosition = new Vector3 ( Random.Range (-3.5f, 3.5f), Random.Range(-1.8f, 1.8f), 0f);
+        Instantiate(foodPrefab, randomPosition, Quaternion.identity);
+    }
+}
---
+++ Scripts/Node.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    Transform parentNode;
+    Vector3 destinationPoint;
+    bool inited;
+
+    public void Init(Transform tr)
+    {
+        parentNode = tr;
+        destinationPoint = tr.position;
+        GetComponent<SpriteRenderer>().color = new Color(1.0f, 0.0f, 0.0f);
+        inited = true;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (inited)
+        {
+            transform.position = transform.position + (destinationPoint - transform.position) * 5f * Time.fixedDeltaTime ;
+            if ( (transform.position - destinationPoint).sqrMagnitude < 0.1f)
+                destinationPoint = parentNode.position;
+        }
+
+    }
+}
